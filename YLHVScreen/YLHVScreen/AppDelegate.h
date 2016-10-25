96eeabe74f8a55cc0b1d5fbcc9ec8508e0c04c28
IOSYUNYUN <liuyun1891@sina.com>
2016-10-25 15:17:47 +0800

    提交最初时的版本

---
+++ YLHVScreen/YLHVScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLHVScreen
+//
+//  Created by 刘云 on 2016/10/25.
+//  Copyright © 2016年 YouLing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLHVScreen/YLHVScreen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YLHVScreen
+//
+//  Created by 刘云 on 2016/10/25.
+//  Copyright © 2016年 YouLing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
